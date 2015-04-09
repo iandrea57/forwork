3c5a4c121d4f65dd51399564bc9e612dbaf9ca14
iAndrea <iiiandrea57@gmail.com>
2015-04-09 18:52:51 +0800

    jdkproxy

---
+++ pro/mmz-forwork-test/src/main/java/com/mmz/forwork/spring/aop/proxy/CglibProxy.java
@@ -0,0 +1,16 @@
+/**
+ * $Id$
+ * Copyright 2011-2013 Oak Pacific Interactive. All rights reserved.
+ */
+package com.mmz.forwork.spring.aop.proxy;
+
+/**
+ * @author <a href="mailto:hailong.peng@renren-inc.com">彭海龙</a>
+ * @createTime 15-4-9 下午6:36
+ */
+public class CglibProxy {
+
+    public static class CglibProxyFactory implements MethodIntercepter {
+
+    }
+}
---
+++ pro/mmz-forwork-test/src/main/java/com/mmz/forwork/spring/aop/proxy/IOffer.java
@@ -0,0 +1,13 @@
+package com.mmz.forwork.spring.aop.proxy;
+
+/**
+ * @author <a href="mailto:hailong.peng@renren-inc.com">彭海龙</a>
+ * @createTime 15-4-9 下午6:32
+ */
+public interface IOffer {
+
+    public void postOffer();
+
+    public void modifyOffer();
+
+}
